ed9df257ba1859901eb9c16f0d9cf931d559001c
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-31 02:00:51 +0000

    fixed setup.py and added test for it added utils/Version.py file added missing __init__.py files

--- osbot_utils/__init__.py
+++ osbot_utils/__init__.py
@@ -1,9 +1,1 @@
-"""
-## OSBot-Utils
-
-Project with multiple Util classes (to streamline development)
-
-Main GitHub repo: https://github.com/owasp-sbot/OSBot-Utils
-
-[![Coverage Status](https://coveralls.io/repos/github/owasp-sbot/OSBot-Utils/badge.svg?branch=master)](https://coveralls.io/github/owasp-sbot/OSBot-Utils?branch=master)
-"""
+path=__path__[0]
