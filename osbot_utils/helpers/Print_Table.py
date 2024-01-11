3b7e33d1986ed80ce6909bee3c89a91582710ef3
Dinis Cruz <dinis.cruz@owasp.org>
2024-01-11 02:43:10 +0000

    improved Print_Table

--- osbot_utils/helpers/Print_Table.py
+++ osbot_utils/helpers/Print_Table.py
@@ -1,5 +1,3 @@
-from osbot_utils.utils.Dev import pprint
-
 from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
 
 
@@ -35,7 +33,6 @@ class Print_Table(Kwargs_To_Self):
     rows                : list
     rows_texts          : list
     table_width         : int
-    #top_separator      : str
     text__all           : list
     text__footer        : str
     text__headers       : list
@@ -43,67 +40,127 @@ class Print_Table(Kwargs_To_Self):
     text__table_middle  : str
     text__table_top     : str
     text__title         : str
+    text__width         : int
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def add_column(self, header, cells:list):
+        self.fix_table()
+        columns_count = len(self.headers)
+        self.add_header(header)
+        for index, cell in enumerate(cells):
+            if len(self.rows) <= index:
+                new_row = ['' for _ in range(columns_count)] + [cell]
+                self.rows.append(new_row)
+            else:
+                self.rows[index].append(cell)
+        return self
+
+    def add_header(self, header:str):
+        self.headers.append(header)
+        return self
+
+    def add_headers(self, *headers:list):
+        for header in headers:
+            self.add_header(header)
+        return self
+
+    def add_row(self, row:list):
+        if type(row) is not list:
+            self.rows.append([row])
+        else:
+            self.rows.append(row)
+        return self
+
+    def add_rows(self, rows:list):
+        for row in rows:
+            self.add_row(row)
+        return self
+
+    def fix_table(self):
+        if self.rows:
+            max_cells = max(len(row) for row in self.rows)          # get max number of cells in any row
+        else:
+            max_cells = 0
+
+        extra_header_count = len(self.headers) + 1                  # Start counting extra headers from the current number of headers
+        while len(self.headers) < max_cells:                        # Extend headers if necessary
+            self.headers.append(f"Header #{extra_header_count}")    # headers cannot have empty values
+            extra_header_count += 1
+        for row in self.rows:                                       # Ensure each row has the same number of cells as there are headers
+            while len(row) < len(self.headers):
+                row.append("")
+
     def map_headers_size(self):
         self.headers_size = [len(header) for header in self.headers]
-        for row in self.rows:                               # Update max width based on row data
+        for row in self.rows:                                                       # Update max width based on row data
             for index, cell in enumerate(row):
                 self.headers_size[index] = max(self.headers_size[index], len(cell))
         return self
 
     def map_table_width(self):
         self.table_width = len(self.text__headers)
+        if len(self.footer) > self.table_width:
+            self.table_width = len(self.footer) + 4
+        if len(self.title) > self.table_width:
+            self.table_width = len(self.title) + 4
+
 
     def map_rows_texts(self):
         self.rows_texts = []
-        for row in self.rows:
-            row_text = CHAR_TABLE_VERTICAL
-            for index, cell in enumerate(row):
-                size = self.headers_size[index]
-                row_text += f" {cell:{size}} {CHAR_TABLE_VERTICAL}"
-            self.rows_texts.append(row_text)
+        if not self.rows:
+            self.rows_texts = [f"{CHAR_TABLE_VERTICAL}  {CHAR_TABLE_VERTICAL}"]
+        else:
+            for row in self.rows:
+                row_text = CHAR_TABLE_VERTICAL
+                for index, cell in enumerate(row):
+                    size = self.headers_size[index]
+                    row_text += f" {cell:{size}} {CHAR_TABLE_VERTICAL}"
+                self.rows_texts.append(row_text)
         return self
 
     def map_text__all(self):
-        self.text__all = [self.text__table_top    ,
-                          self.text__title        ,
-                          self.text__table_middle ,
-                          self.text__headers      ,
-                          self.text__table_middle ,
-                          *self.rows_texts        ,
-                          self.text__table_middle ,
-                          self.text__footer       ,
-                          self.text__table_bottom ]
+        self.text__all                      = [  self.text__table_top                              ]
+        if self.title   :   self.text__all += [  self.text__title        , self.text__table_middle ]
+        if self.headers :   self.text__all += [  self.text__headers      , self.text__table_middle ]
+        if self.rows    :   self.text__all += [ *self.rows_texts                                   ]
+        if self.footer  :   self.text__all += [  self.text__table_middle , self.text__footer       ]
+        self.text__all                     += [  self.text__table_bottom                           ]
 
     def map_text__footer(self):
-        self.text__footer = f"{CHAR_TABLE_VERTICAL} {self.footer:{self.table_width - 4}} {CHAR_TABLE_VERTICAL}"
+        self.text__footer = f"{CHAR_TABLE_VERTICAL} {self.footer:{self.text__width}} {CHAR_TABLE_VERTICAL}"
 
     def map_text__headers(self):
         self.text__headers = CHAR_TABLE_VERTICAL
-        for header, size in zip(self.headers, self.headers_size):
-            self.text__headers += f" {header:{size}} {CHAR_TABLE_VERTICAL}"
-        return self
+        if not self.headers:
+            self.text__headers += f"  {CHAR_TABLE_VERTICAL}"
+        else:
+            for header, size in zip(self.headers, self.headers_size):
+                self.text__headers += f" {header:{size}} {CHAR_TABLE_VERTICAL}"
+            return self
 
-    def map_text__table_bottom(self):
-        self.text__table_bottom = f"{CHAR_TABLE_BOTTOM_LEFT}" + CHAR_TABLE_HORIZONTAL * (self.table_width - 2) + f"{CHAR_TABLE_BOTTOM_RIGHT}"
-
-    def map_text__table_middle(self):
-        self.text__table_middle = f"{CHAR_TABLE_MIDDLE_LEFT}" + CHAR_TABLE_HORIZONTAL * (self.table_width - 2) + f"{CHAR_TABLE_MIDDLE_RIGHT}"
-
-    def map_text__table_top(self):
-        self.text__table_top = f"{CHAR_TABLE_TOP_LEFT}" + CHAR_TABLE_HORIZONTAL * (self.table_width - 2) + f"{CHAR_TABLE_TOP_RIGHT}"
+    def map_text__table_bottom(self):   self.text__table_bottom = f"{CHAR_TABLE_BOTTOM_LEFT}" + CHAR_TABLE_HORIZONTAL * (self.text__width + 2) + f"{CHAR_TABLE_BOTTOM_RIGHT }"
+    def map_text__table_middle(self):   self.text__table_middle = f"{CHAR_TABLE_MIDDLE_LEFT}" + CHAR_TABLE_HORIZONTAL * (self.text__width + 2) + f"{CHAR_TABLE_MIDDLE_RIGHT }"
+    def map_text__table_top   (self):   self.text__table_top    = f"{CHAR_TABLE_TOP_LEFT   }" + CHAR_TABLE_HORIZONTAL * (self.text__width + 2) + f"{CHAR_TABLE_TOP_RIGHT    }"
 
     def map_text__title(self):
-        self.text__title = f"{CHAR_TABLE_VERTICAL} {self.title:{self.table_width - 4}} {CHAR_TABLE_VERTICAL}"
+        self.text__title = f"{CHAR_TABLE_VERTICAL} {self.title:{self.text__width}} {CHAR_TABLE_VERTICAL}"
+
+    def map_text__width(self):
+        self.text__width = self.table_width - 4
+        # if self.table_width > 3:                                      # there is no use case that that needs this check
+        #     self.text__width = self.table_width - 4
+        # else:
+        #     self.text__width = 0
 
     def map_texts(self):
+        self.fix_table              ()
         self.map_headers_size       ()
         self.map_text__headers      ()
         self.map_rows_texts         ()
         self.map_table_width        ()
+        self.map_text__width        ()
         self.map_text__footer       ()
         self.map_text__title        ()
         self.map_text__table_bottom ()
@@ -111,8 +168,29 @@ class Print_Table(Kwargs_To_Self):
         self.map_text__table_top    ()
         self.map_text__all          ()
 
+
     def print(self):
+        print()
         self.map_texts()
         for text in self.text__all:
             print(text)
 
+    def escape_csv_cell(self, cell):
+        if cell and any(c in cell for c in [',', '"', '\n']):
+            cell = cell.replace('"', '""')              # Escape double quotes
+            cell = cell.replace('\n', '\\n')            # escape new lines
+            return f'"{cell}"'                          # Enclose the cell in double quotes
+        return cell
+
+    def to_csv(self):
+        csv_content = ','.join(self.escape_csv_cell(header) for header in self.headers) + '\n'          # Create a CSV string from the headers and rows
+        for row in self.rows:
+            csv_content += ','.join(self.escape_csv_cell(cell) if cell is not None else '' for cell in row) + '\n'
+        return csv_content
+
+    def to_dict(self):
+        table_dict = {header: [] for header in self.headers}                                # Initialize the dictionary with empty lists for each header
+        for row in self.rows:                                                               # Iterate over each row and append the cell to the corresponding header's list
+            for header, cell in zip(self.headers, row):
+                table_dict[header].append(cell)
+        return table_dict
--- tests/helpers/test_Print_Table.py
+++ tests/helpers/test_Print_Table.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from unittest.mock import patch, call
-
 from osbot_utils.helpers.Print_Table import Print_Table
 
 
@@ -9,6 +8,198 @@ class test_Print_Table(TestCase):
     def setUp(self):
         self.print_table = Print_Table()
 
+    def test___init__(self):
+        assert self.print_table.__locals__() == { 'footer'              : '' ,
+                                                  'headers'             : [] ,
+                                                  'headers_size'        : [] ,
+                                                  'rows'                : [] ,
+                                                  'rows_texts'          : [] ,
+                                                  'table_width'         : 0  ,
+                                                  'text__all'           : [] ,
+                                                  'text__footer'        : '' ,
+                                                  'text__headers'       : [] ,
+                                                  'text__table_bottom'  : '' ,
+                                                  'text__table_middle'  : '' ,
+                                                  'text__table_top'     : '' ,
+                                                  'text__title'         : '' ,
+                                                  'text__width'         : 0  ,
+                                                  'title'               : '' }
+
+    def test_add_add_column(self):
+        with self.print_table as _:
+            _.add_column(header='header_1', cells=['cell_1', 'cell_2', 'cell_3'])
+            _.map_texts()
+            assert _.to_dict() == {'header_1': ['cell_1', 'cell_2', 'cell_3']}
+
+            _.add_header('header_2')
+            _.map_texts()
+            assert _.to_dict() == {'header_1': ['cell_1', 'cell_2', 'cell_3'],
+                                   'header_2': ['', '', '']}
+
+            _.add_column(header='header_3',cells= ['cell_4', 'cell_5', 'cell_6'])
+            _.map_texts()
+            assert _.to_dict() == {'header_1': ['cell_1', 'cell_2', 'cell_3'],
+                                  'header_2': ['', '', ''],
+                                  'header_3': ['cell_4', 'cell_5', 'cell_6']} != {'header_1': ['cell_1', 'cell_2', 'cell_3'], 'header_2': ['', '', '']}
+            _.add_row(['cell_7', 'cell_8', 'cell_9'])
+            _.add_row('cell_10')
+            _.add_column(header='header_4', cells= ['cell_11', 'cell_12', 'cell_13'])
+            assert _.to_dict() == {'header_1': ['cell_1', 'cell_2', 'cell_3', 'cell_7', 'cell_10'],
+                                   'header_2': ['', '', '', 'cell_8', ''],
+                                   'header_3': ['cell_4', 'cell_5', 'cell_6', 'cell_9', ''],
+                                   'header_4': ['cell_11', 'cell_12', 'cell_13']}
+
+            _.add_column(header='header_5', cells=['cell_14', 'cell_15', 'cell_16', 'cell_17', 'cell_18', 'cell_19'])
+            assert _.to_dict() == {'header_1': ['cell_1', 'cell_2', 'cell_3', 'cell_7', 'cell_10', ''],
+                                   'header_2': ['', '', '', 'cell_8', '', ''],
+                                   'header_3': ['cell_4', 'cell_5', 'cell_6', 'cell_9', '', ''],
+                                   'header_4': ['cell_11', 'cell_12', 'cell_13', '', '', ''],
+                                   'header_5': ['cell_14', 'cell_15', 'cell_16', 'cell_17', 'cell_18', 'cell_19']}
+            _.map_texts()
+            assert _.text__all == ['┌──────────────────────────────────────────────────────┐',
+                                   '│ header_1 │ header_2 │ header_3 │ header_4 │ header_5 │',
+                                   '├──────────────────────────────────────────────────────┤',
+                                   '│ cell_1   │          │ cell_4   │ cell_11  │ cell_14  │',
+                                   '│ cell_2   │          │ cell_5   │ cell_12  │ cell_15  │',
+                                   '│ cell_3   │          │ cell_6   │ cell_13  │ cell_16  │',
+                                   '│ cell_7   │ cell_8   │ cell_9   │          │ cell_17  │',
+                                   '│ cell_10  │          │          │          │ cell_18  │',
+                                   '│          │          │          │          │ cell_19  │',
+                                   '└──────────────────────────────────────────────────────┘']
+
+            _.add_row([' a\'c ', '  c"d ' , ' e\nf ' ] )
+            assert _.to_csv() == ('header_1,header_2,header_3,header_4,header_5\n'
+                                  'cell_1,,cell_4,cell_11,cell_14\n'
+                                  'cell_2,,cell_5,cell_12,cell_15\n'
+                                  'cell_3,,cell_6,cell_13,cell_16\n'
+                                  'cell_7,cell_8,cell_9,,cell_17\n'
+                                  'cell_10,,,,cell_18\n'
+                                  ',,,,cell_19\n'
+                                  ' a\'c ,"  c""d "," e\\nf "\n')
+
+    def test_add_headers(self):
+        headers = ['header_1', 'header_2', 'header_3']
+        with self.print_table as _:
+            _.add_headers(*headers)
+            _.map_texts()
+            assert _.text__all == ['┌────────────────────────────────┐',
+                                   '│ header_1 │ header_2 │ header_3 │',
+                                   '├────────────────────────────────┤',
+                                   '└────────────────────────────────┘']
+    def test_add_row(self):
+        headers = ['header_1', 'header_2', 'header_3']
+        row   = ['cell_1', 'cell_2']
+        with self.print_table as _:
+            _.add_headers(*headers)
+            _.add_row(row)
+            _.map_texts()
+            assert _.text__all == ['┌────────────────────────────────┐',
+                                   '│ header_1 │ header_2 │ header_3 │',
+                                   '├────────────────────────────────┤',
+                                   '│ cell_1   │ cell_2   │          │',
+                                   '└────────────────────────────────┘']
+
+    def test_add_rows(self):
+        row_1 = ['cell_1'                    ]
+        row_2 = ['cell_2', 'cell_3'          ]
+        row_3 = ['cell_4', 'cell_5', 'cell_6']
+        row_4 = ['cell_7', 'cell_8'          ]
+        rows  = [row_1, row_2, row_3, row_4]
+        with self.print_table as _:
+            _.add_rows(rows)
+            _.map_texts()
+            assert _.to_dict() == {'Header #1': ['cell_1', 'cell_2', 'cell_4', 'cell_7'],
+                                   'Header #2': ['', 'cell_3', 'cell_5', 'cell_8'],
+                                   'Header #3': ['', '', 'cell_6', '']}
+
+            assert _.text__all == ['┌───────────────────────────────────┐',
+                                   '│ Header #1 │ Header #2 │ Header #3 │',
+                                   '├───────────────────────────────────┤',
+                                   '│ cell_1    │           │           │',
+                                   '│ cell_2    │ cell_3    │           │',
+                                   '│ cell_4    │ cell_5    │ cell_6    │',
+                                   '│ cell_7    │ cell_8    │           │',
+                                   '└───────────────────────────────────┘']
+
+    def test_fix_table(self):
+        with self.print_table as _:
+            _.add_row('cell_1')
+            _.map_texts()
+            assert _.to_dict() == {'Header #1': ['cell_1']}
+            _.add_row(['cell_2', 'cell_3'])
+            _.map_texts()
+            assert _.to_dict() == {'Header #1': ['cell_1', 'cell_2'], 'Header #2': ['', 'cell_3']}
+            assert _.text__all == ['┌───────────────────────┐',
+                                   '│ Header #1 │ Header #2 │',
+                                   '├───────────────────────┤',
+                                   '│ cell_1    │           │',
+                                   '│ cell_2    │ cell_3    │',
+                                   '└───────────────────────┘']
+            _.add_header('Header #3')
+            assert _.to_dict() == {'Header #1': ['cell_1', 'cell_2'],
+                                   'Header #2': ['', 'cell_3'],
+                                   'Header #3': []}
+            _.map_texts()
+            assert _.text__all == ['┌───────────────────────────────────┐',
+                                   '│ Header #1 │ Header #2 │ Header #3 │',
+                                   '├───────────────────────────────────┤',
+                                   '│ cell_1    │           │           │',
+                                   '│ cell_2    │ cell_3    │           │',
+                                   '└───────────────────────────────────┘']
+            _.add_header('some name')
+            _.add_row(['cell_4', 'cell_5', 'cell_6', 'cell_7', 'cell_8'])
+
+            _.map_texts()
+            assert _.to_dict() == {'Header #1': ['cell_1', 'cell_2', 'cell_4'],
+                                   'Header #2': ['', 'cell_3', 'cell_5'],
+                                   'Header #3': ['', '', 'cell_6'],
+                                   'Header #5': ['', '', 'cell_8'],                     # note that this dict is not ordered
+                                   'some name': ['', '', 'cell_7']}
+
+            assert _.text__all == ['┌───────────────────────────────────────────────────────────┐',
+                                   '│ Header #1 │ Header #2 │ Header #3 │ some name │ Header #5 │',
+                                   '├───────────────────────────────────────────────────────────┤',
+                                   '│ cell_1    │           │           │           │           │',
+                                   '│ cell_2    │ cell_3    │           │           │           │',
+                                   '│ cell_4    │ cell_5    │ cell_6    │ cell_7    │ cell_8    │',
+                                   '└───────────────────────────────────────────────────────────┘']
+
+
+    def test_map_texts(self):
+        with  self.print_table as _:
+            assert _.text__all == []
+            _.map_texts()
+            assert _.text__all == ['┌──┐', '└──┘']
+            with patch("builtins.print") as _:
+                self.print_table.print()
+                assert _.call_args_list == [call(), call('┌──┐'), call('└──┘')]
+
+
+    def test_map_text__footer(self):
+        with  self.print_table as _:
+            _.footer = 'an footer value'
+            _.map_texts()
+            assert _.text__all == ['┌─────────────────┐',
+                                   '├─────────────────┤',
+                                   '│ an footer value │',
+                                   '└─────────────────┘']
+
+            with patch("builtins.print") as _:
+                self.print_table.print()
+                assert _.call_args_list == [ call(),
+                                             call('┌─────────────────┐'),
+                                             call('├─────────────────┤'),
+                                             call('│ an footer value │'),
+                                             call('└─────────────────┘')]
+
+    def test_map_text__title(self):
+        with  self.print_table as _:
+            _.title = 'an title value'
+            _.map_texts()
+            assert _.text__all == ['┌────────────────┐',
+                                   '│ an title value │',
+                                   '├────────────────┤',
+                                   '└────────────────┘']
     def test_print(self):
         with self.print_table as _:
             _.title        =  "BOTO3 REST calls (via BaseClient._make_api_call)"
@@ -23,7 +214,8 @@ class test_Print_Table(TestCase):
         with patch("builtins.print") as _:
             self.print_table.print()
             #assert len(_.call_args_list) == 12
-            assert _.call_args_list == [ call('┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐'),
+            assert _.call_args_list == [ call(),
+                                         call('┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐'),
                                          call('│ BOTO3 REST calls (via BaseClient._make_api_call)                                                                                  │'),
                                          call('├───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤'),
                                          call('│ # │ Method            │ Duration │ Params                      │ Return Value                                                     │'),
@@ -35,4 +227,11 @@ class test_Print_Table(TestCase):
                                          call('│ Total Duration:   0.73 secs | Total calls: 3                                                                                      │'),
                                          call('└───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘')]
 
-
+    def test_map_text__width(self):
+        with self.print_table as _:
+            assert _.text__width   == 0                 # check conner case of that used to happen when: self.table_width < 4
+            assert _.table_width   == 0
+            _.map_texts()
+            assert _.table_width   == 4
+            assert _.text__width   == 0
+            assert _.text__headers == '│  │'
