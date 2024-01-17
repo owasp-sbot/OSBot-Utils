6e68ceef4c7888af0435f25ce019a124d53ac164
Dinis Cruz <dinis.cruz@owasp.org>
2024-01-17 00:49:34 +0000

    Improve code coverage

--- tests/helpers/trace/test_Trace_Call__Config.py
+++ tests/helpers/trace/test_Trace_Call__Config.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
+from unittest.mock import patch, call
 
 from osbot_utils.utils.Objects                    import default_value
-from osbot_utils.helpers.trace.Trace_Call__Config import Trace_Call__Config
+from osbot_utils.helpers.trace.Trace_Call__Config import Trace_Call__Config, DEFAULT_PADDING_DURATION
 
 
 class test_Trace_Call__Config(TestCase):
@@ -50,3 +51,80 @@ class test_Trace_Call__Config(TestCase):
             assert _.__kwargs__         () == expected_data
             expected_data['__lock_attributes__'] = True
             assert _.__locals__         () == expected_data
+
+    def test_all(self):
+        with self.trace_call_config as _:
+            _.all(up_to_depth=5, print_traces=True)
+            assert _.trace_capture_all      is True
+            assert _.print_traces_on_exit   is True
+            assert _.trace_up_to_depth      == 5
+
+    def test_capture(self):
+        with self.trace_call_config as _:
+            assert _.trace_capture_start_with == []
+            assert _.trace_capture_contains   == []
+            assert _.ignore_start_with        == []
+            assert _.print_traces_on_exit     is False
+
+            _.print_traces_on_exit = False
+            _.capture(starts_with=['a,b,c'], contains=['d,e,f'], ignore=['g,h,i'])
+
+            assert _.trace_capture_start_with == ['a,b,c']
+            assert _.trace_capture_contains   == ['d,e,f']
+            assert _.ignore_start_with        == ['g,h,i']
+            assert _.print_traces_on_exit     is True
+
+            _.capture(starts_with='A', contains='B', ignore="C")
+
+            assert _.trace_capture_start_with == ['A']
+            assert _.trace_capture_contains   == ['B']
+            assert _.ignore_start_with        == ['C']
+            assert _.print_traces_on_exit     is True
+
+    def test_duration(self):
+        with self.subTest("Initial state"):
+            config = self.trace_call_config
+            assert config.capture_duration is False
+            assert config.print_duration is False
+            assert config.padding_duration == DEFAULT_PADDING_DURATION
+            assert config.with_duration_bigger_than == 0
+
+        with self.subTest("After setting duration"):
+            config.duration(bigger_than=10, padding=20)
+            assert config.capture_duration is True
+            assert config.print_duration is True
+            assert config.padding_duration == 20
+            assert config.with_duration_bigger_than == 10
+
+    def test_locals(self):
+        config = self.trace_call_config
+        assert config.capture_locals is False
+        assert config.print_locals is False
+
+        config.locals()
+        assert config.capture_locals is True
+        assert config.print_locals is True
+
+    def test_lines(self):
+        config = self.trace_call_config
+        assert config.trace_capture_lines is False
+        assert config.print_traces_on_exit is False
+        assert config.print_lines_on_exit is False
+
+        config.lines(print_traces=True, print_lines=True)
+        assert config.trace_capture_lines is True
+        assert config.print_traces_on_exit is True
+        assert config.print_lines_on_exit is True
+
+    def test_print_config(self):
+        with patch('osbot_utils.helpers.trace.Trace_Call__Config.pprint') as mock_pprint:
+            self.trace_call_config.print_config()
+        mock_pprint.assert_called_once()
+        assert mock_pprint.call_args_list == [call(self.trace_call_config.__locals__())]
+
+    def test_up_to_depth(self):
+        config = self.trace_call_config
+        assert config.trace_up_to_depth == 0
+
+        config.up_to_depth(5)
+        assert config.trace_up_to_depth == 5
