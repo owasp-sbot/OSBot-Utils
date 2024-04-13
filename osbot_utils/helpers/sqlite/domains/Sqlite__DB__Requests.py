2fca1ef1c10e6d761c95ac6c84b32111cb5ffc7d
Dinis Cruz <dinis.cruz@owasp.org>
2024-04-13 13:05:46 +0100

    add support for controlling the table_name in Sqlite__DB__Requests

--- osbot_utils/helpers/sqlite/domains/Sqlite__DB__Requests.py
+++ osbot_utils/helpers/sqlite/domains/Sqlite__DB__Requests.py
@@ -8,8 +8,8 @@ class Sqlite__DB__Requests(Sqlite__DB__Local):
     table_name  : str
     table_schema: type
 
-    def __init__(self,db_path=None, db_name=None):
-        self.db_name     = db_name
-        self.table_name  = SQLITE_TABLE__REQUESTS
+    def __init__(self,db_path=None, db_name=None, table_name=None):
+        self.db_name      = db_name
+        self.table_name   = table_name or SQLITE_TABLE__REQUESTS
         self.table_schema = Schema__Table__Requests
         super().__init__(db_path=db_path)
