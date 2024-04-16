8319a148ab54aed37f0b1a870426b57f6c61ff9d
Dinis Cruz <dinis.cruz@owasp.org>
2024-04-16 22:12:03 +0100

    added add_file and files methods to Sqlite__DB__Files

--- osbot_utils/helpers/sqlite/domains/Sqlite__DB__Files.py
+++ osbot_utils/helpers/sqlite/domains/Sqlite__DB__Files.py
@@ -8,9 +8,15 @@ class Sqlite__DB__Files(Sqlite__DB__Local):
     def __init__(self, db_path=None, db_name=None):
         super().__init__(db_path=db_path, db_name=db_name)
 
+    def add_file(self, path, contents=None, metadata=None):
+        return self.table_files().add_file(path, contents, metadata)
+
     @cache_on_self
     def table_files(self):
         return Sqlite__Table__Files(database=self).setup()
+
+    def files(self):
+        return self.table_files().files()
 
     def setup(self):
         self.table_files()
