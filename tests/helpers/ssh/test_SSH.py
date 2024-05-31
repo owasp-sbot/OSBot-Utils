1915d0205572207dffc0aa8905a69d3f393ddc88
Dinis Cruz <dinis.cruz@owasp.org>
2024-05-31 20:54:24 +0100

    adding fix for error that was failing in GH Actions

--- tests/helpers/ssh/test_SSH.py
+++ tests/helpers/ssh/test_SSH.py
@@ -5,7 +5,7 @@ from osbot_utils.helpers.ssh.SSH  import SSH
 from osbot_utils.helpers.ssh.TestCase__SSH import TestCase__SSH
 from osbot_utils.utils.Dev        import pprint
 from osbot_utils.utils.Env import load_dotenv
-from osbot_utils.utils.Misc       import list_set
+from osbot_utils.utils.Misc import list_set, in_github_action
 
 ENV_VAR_TEST_OSBOT__SSH_HOST      = 'SSH__HOST'
 ENV_VAR_TEST_OSBOT__SSH_KEY_FILE  = 'SSH__KEY_FILE__FILE'
@@ -33,21 +33,25 @@ class test_SSH(TestCase__SSH):
                                       'strict_host_check' : False                 }
 
     def test_execute_command(self):
-        result_ssh_host_remove = self.ssh.remove_server_ssh_host_fingerprint()          # todo: find a better place to run this
-        pprint(result_ssh_host_remove)                                                  #       doing this here because we were getting this in the execute_command below:   'stderr': "Warning: Permanently added '[localhost]:22222' (ED25519) to the list of "
+        # result_ssh_host_remove = self.ssh.remove_server_ssh_host_fingerprint()          # todo: find a to handle the case when the
+        # pprint(result_ssh_host_remove)                                                  #       /home/runner/.ssh/known_hosts doesn't exist or has changed
 
         assert self.ssh.execute_command(None) == { 'data' : None, 'message': '', 'status': 'error' ,
                                                    'error': 'in execute_command not all required vars were setup'}
         command = 'uname'
         ssh_args = self.ssh.execute_command_args(command)
         result   = self.ssh.execute_command(command)
+        if in_github_action():
+            stderr   =  "Warning: Permanently added '[localhost]:22222' (ED25519) to the list of known hosts.\r\n"
+        else:
+            stderr   = ''
         assert result == { 'cwd'      : '.'                   ,
                            'duration' : result.get('duration'),
                            'error'    : None                  ,
                            'kwargs'   : {'cwd': '.', 'stderr': -1, 'stdout': -1, 'timeout': None},
                            'runParams': ['ssh'] + ssh_args    ,
                            'status'   : 'ok'                  ,
-                           'stderr'   : ''                    ,
+                           'stderr'   : stderr                ,
                            'stdout'   : 'Linux\n'             }
 
     def test_execute_command_args(self):
