298abfcc8292190d9a6c200f193f8d9578dba83d
Dinis Cruz <dinis.cruz@owasp.org>
2024-09-11 13:22:31 +0100

    minor hot-fix to Temp_Env_Vars

--- osbot_utils/testing/Temp_Env_Vars.py
+++ osbot_utils/testing/Temp_Env_Vars.py
@@ -1,6 +1,6 @@
 import os
-
 from osbot_utils.base_classes.Type_Safe import Type_Safe
+from osbot_utils.utils.Env              import del_env, set_env
 
 
 class Temp_Env_Vars(Type_Safe):
@@ -16,12 +16,12 @@ class Temp_Env_Vars(Type_Safe):
     def set_vars(self):
         for key, value in self.env_vars.items():
             self.original_env_vars[key] = os.environ.get(key)                   # Backup original environment variables and set new ones
-            os.environ[key] = value
+            set_env(key, value)
         return self
 
     def restore_vars(self):
         for key in self.env_vars:                                               # Restore original environment variables
             if self.original_env_vars[key] is None:
-                del os.environ[key]
+                del_env(key)
             else:
-                os.environ[key] = self.original_env_vars[key]
+                set_env(key, self.original_env_vars[key])
