6cb52570504909514ea0eeb5cdd4fe4bc2cf1b2f
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-15 22:29:27 +0100

    removing for now the flow_id variable from Flow_Run__Event_Data since it was not being used

--- osbot_utils/helpers/flows/models/Flow_Run__Event_Data.py
+++ osbot_utils/helpers/flows/models/Flow_Run__Event_Data.py
@@ -6,7 +6,7 @@ from osbot_utils.base_classes.Type_Safe import Type_Safe
 class Flow_Run__Event_Data(Type_Safe):
     data        : dict
     event_source: str
-    flow_id     : str = None
+    #flow_id     : str = None           # todo: add support for capturing the actual flow_idq
     flow_name   : str = None
     flow_run_id : str = None
     log_level   : int = logging.INFO
