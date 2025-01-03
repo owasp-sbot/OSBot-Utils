d156255a635e9910be5433937721231383c1dc9f
Dinis Cruz <dinis.cruz@owasp.org>
2025-01-03 12:26:41 +0000

    fixed test

--- tests/unit/type_safe/bugs/test_Type_Safe__Dict__bugs.py
+++ tests/unit/type_safe/bugs/test_Type_Safe__Dict__bugs.py
@@ -1,4 +1,5 @@
-from typing                                 import Any
+import pytest
+from typing                                 import Any, Dict
 from unittest                               import TestCase
 from osbot_utils.type_safe.Type_Safe        import Type_Safe
 from osbot_utils.type_safe.Type_Safe__Dict  import Type_Safe__Dict
@@ -78,3 +79,13 @@ class test_Type_Safe__Dict__bugs(TestCase):
             "mixed": ({"value": "test"}, 2, {"value": "other"})
         }
         assert safe_dict.json() != expected                                     # BUG should be equal
+
+    def test__bug__obj__not_supported(self):
+        class An_Class(Type_Safe):
+            an_dict: Dict[str, str]
+
+        an_class = An_Class()
+        an_class.an_dict['a'] = 'b'
+        assert an_class.an_dict.json() == {'a':'b'}
+        with pytest.raises(AttributeError, match= "Type_Safe__Dict' object has no attribute 'obj'"):
+            an_class.an_dict.obj()           # BUG
