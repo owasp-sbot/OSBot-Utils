5b4c2e17cfc19c7630c0cedf69ad7a253af2a040
Dinis Cruz <dinis.cruz@owasp.org>
2025-01-18 17:05:20 +0000

    updated timings for performance tests in GH

--- tests/unit/testing/performance/test_Performance_Measure__Session.py
+++ tests/unit/testing/performance/test_Performance_Measure__Session.py
@@ -1,4 +1,7 @@
 from unittest                                                       import TestCase
+
+from osbot_utils.utils.Env import in_github_action
+
 from osbot_utils.helpers.Random_Guid                                import Random_Guid
 from osbot_utils.testing.performance.Performance_Measure__Session   import Performance_Measure__Session
 from osbot_utils.type_safe.Type_Safe                                import Type_Safe
@@ -25,12 +28,22 @@ class test_Performance_Checks__Session(TestCase):
         class An_Class_6(Type_Safe):
             an_str: str = '42'
 
-        print()
-        Performance_Measure__Session().measure(str        ).print()#.assert_time(  100)
-        Performance_Measure__Session().measure(Random_Guid).print()#.assert_time( 6000)
-        Performance_Measure__Session().measure(An_Class_1 ).print()#.assert_time(  100)
-        Performance_Measure__Session().measure(An_Class_2 ).print()#.assert_time( 6000)
-        Performance_Measure__Session().measure(An_Class_3 ).print()#.assert_time(20000)
-        Performance_Measure__Session().measure(An_Class_4 ).print()#.assert_time(20000)
-        Performance_Measure__Session().measure(An_Class_5 ).print()#.assert_time(20000)
-        Performance_Measure__Session().measure(An_Class_6 ).print()#.assert_time(20000)
+        if in_github_action():
+            time_slow = 200
+            time_mid_1 = 10000
+            time_mid_2 = 20000
+            time_large = 50000
+        else:
+            time_slow = 100
+            time_mid_1 = 6000
+            time_mid_2 = 6000
+            time_large = 20000
+
+        Performance_Measure__Session().measure(str        ).print().assert_time(time_slow )
+        Performance_Measure__Session().measure(Random_Guid).print().assert_time(time_mid_1)
+        Performance_Measure__Session().measure(An_Class_1 ).print().assert_time(time_slow )
+        Performance_Measure__Session().measure(An_Class_2 ).print().assert_time(time_mid_2)
+        Performance_Measure__Session().measure(An_Class_3 ).print().assert_time(time_large)
+        Performance_Measure__Session().measure(An_Class_4 ).print().assert_time(time_large)
+        Performance_Measure__Session().measure(An_Class_5 ).print().assert_time(time_large)
+        Performance_Measure__Session().measure(An_Class_6 ).print().assert_time(time_large)
