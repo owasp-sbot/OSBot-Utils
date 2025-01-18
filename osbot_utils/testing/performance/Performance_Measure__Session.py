3fae64370e9e17869ee5141d812690aa12d61afe
Dinis Cruz <dinis.cruz@owasp.org>
2025-01-18 20:55:04 +0000

    fixed GH test

--- osbot_utils/testing/performance/Performance_Measure__Session.py
+++ osbot_utils/testing/performance/Performance_Measure__Session.py
@@ -100,5 +100,5 @@ class Performance_Measure__Session(Type_Safe):
         if in_github_action():
             first_expected_time = expected_time[0]
             new_expected_time   = first_expected_time *2
-            assert first_expected_time <  self.result.final_score < new_expected_time, f"Performance changed for {self.result.name}: got {self.result.final_score:,d}ns, expected {expected_time}"
+            assert first_expected_time <=  self.result.final_score <= new_expected_time, f"Performance changed for {self.result.name}: got {self.result.final_score:,d}ns, expected {expected_time}"
         assert self.result.final_score in expected_time,  f"Performance changed for {self.result.name}: got {self.result.final_score:,d}ns, expected {expected_time}"
