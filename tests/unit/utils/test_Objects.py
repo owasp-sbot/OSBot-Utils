819ede74441283224b6326194917314991558e1c
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-07 02:57:10 +0100

    minor formatting changes

--- tests/unit/utils/test_Objects.py
+++ tests/unit/utils/test_Objects.py
@@ -1,26 +1,22 @@
 import os
 import sys
 import types
-from enum import Enum
-
 import pytest
 import unittest
 from collections.abc                                                    import Mapping
-from typing import Optional, Union, List, Any, Dict, Set
+from enum                                                               import Enum
+from typing                                                             import Optional, Union, List, Any, Dict, Set
 from unittest                                                           import TestCase
 from unittest.mock                                                      import patch, call
-
-from osbot_utils.helpers.duration.decorators.capture_duration import capture_duration
-from osbot_utils.helpers.duration.decorators.print_duration import print_duration
 from osbot_utils.testing.Stdout                                         import Stdout
 from osbot_utils.type_safe.Type_Safe                                    import Type_Safe
-from osbot_utils.type_safe.primitives.safe_str.llm.Enum__LLM__Role import Enum__LLM__Role
-from osbot_utils.type_safe.type_safe_core.collections.Type_Safe__Dict import Type_Safe__Dict
-from osbot_utils.type_safe.type_safe_core.collections.Type_Safe__List import Type_Safe__List
+from osbot_utils.type_safe.primitives.safe_str.llm.Enum__LLM__Role      import Enum__LLM__Role
+from osbot_utils.type_safe.type_safe_core.collections.Type_Safe__Dict   import Type_Safe__Dict
+from osbot_utils.type_safe.type_safe_core.collections.Type_Safe__List   import Type_Safe__List
 from osbot_utils.type_safe.type_safe_core.shared.Type_Safe__Convert     import type_safe_convert
 from osbot_utils.type_safe.type_safe_core.shared.Type_Safe__Validation  import type_safe_validation
 from osbot_utils.utils.Misc                                             import random_int, list_set
-from osbot_utils.utils.Objects import class_name, get_field, get_value, obj_get_value, obj_values, obj_keys, obj_items, obj_dict, default_value, base_classes, \
+from osbot_utils.utils.Objects                                          import class_name, get_field, get_value, obj_get_value, obj_values, obj_keys, obj_items, obj_dict, default_value, base_classes, \
     class_functions_names, class_functions, dict_remove, class_full_name, get_missing_fields, \
     print_object_methods, print_obj_data_aligned, obj_data, print_obj_data_as_dict, print_object_members, \
     obj_base_classes, obj_base_classes_names, type_mro, pickle_save_to_bytes, pickle_load_from_bytes, dict_to_obj, obj_to_dict, __, serialize_to_dict
